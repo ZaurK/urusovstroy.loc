47f9694a4490123ec0557f7fde8b2556f4536207
Zaur <zaurpost@yandex.ru>
2018-06-11 09:42:16 +0300

    Update scroll.js

--- web/js/scroll.js
+++ web/js/scroll.js
@@ -1,26 +1,50 @@
 $(function(){
 
 $('a#adesignproject').on('click', function(){
+	if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#design-project').offset().top }, 1000);
 });
 
 $('a#aremontproject').on('click', function(){
+if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#remont-project').offset().top }, 1000);
 });
 
 $('a#aadvantages').on('click', function(){
+	if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#advantages').offset().top }, 1000);
 });
 
 $('a#avideorow').on('click', function(){
+	if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#videorow').offset().top }, 1000);
 });
 
 $('a#aphotorow').on('click', function(){
+	if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#photorow').offset().top }, 1000);
 });
 
 $('a#zzfooter').on('click', function(){
+	if ( $('body').hasClass('offcanvas') ) {
+    			$('body').removeClass('offcanvas');
+    			$('.js-zz-nav-toggle').removeClass('active');				
+	    	}
    $('html,body').stop().animate({ scrollTop: $('#zz-footer').offset().top }, 1000);
 });
 
